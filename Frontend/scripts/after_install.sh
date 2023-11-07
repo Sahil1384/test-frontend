0ba00b667fa84fa4e67c3ab506f00298ed701688
Sahil1384 <sahil.techtweekinfotech@gmail.com>
2023-11-07 15:02:47 +0530

    update 21

--- Frontend/scripts/after_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/final_node_code/Frontend
-
-npm install 
-#npm install -g pm2 # may require sudo
-
--- Frontend/scripts/application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/final_node_code/Frontend
-
-
-npm run start
